62b526d42655d7626ffaf2a7bdbb8eeb1a136337
Meagle <mindfulyeon@gmail.com>
2020-08-21 11:33:02 +0900

    Games 컴포넌트 > 이미지 추가

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Styled from "../Games/index.styles";
+import motif from "../../images/game-collection/motif-blue.svg";
 
 const index = () => {
-  return <Styled.Games></Styled.Games>;
+  return (
+    <Styled.Games>
+      <img src={motif} className="motif" alt="game-motif" />
+    </Styled.Games>
+  );
 };
 
 export default index;
--- src/components/Games/index.styles.js
+++ src/components/Games/index.styles.js
@@ -4,6 +4,14 @@ const Games = styled.section`
   background: #fafafa;
   width: 100%;
   height: 79vh;
+  position: relative;
+
+  > .motif {
+    position: absolute;
+    width: 180px;
+    top: 74px;
+    left: 23px;
+  }
 `;
 
 export default { Games };
