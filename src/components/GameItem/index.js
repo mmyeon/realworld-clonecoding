e04a9df7c84b1c7acc971fa218735428650bfaf1
Meagle <mindfulyeon@gmail.com>
2020-08-21 17:25:46 +0900

    GameItem 컴포넌트 > 게임 아이템 나열, 디자인 추가

--- src/components/GameItem/index.js
+++ src/components/GameItem/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import Styled from "./index.styles.js";
 import gameImage from "../../images/game-collection/agent-x.png";
+import star from "../../images/game-collection/star.svg";
 
 const index = () => {
   return (
     <Styled.Container>
-      <img src={gameImage} className="game-image" alt="gameImage" />
+      <div className="item">
+        <img src={gameImage} className="game-image" alt="gameImage" />
+        <div className="detail-content">
+          <span className="location">서울숲 일대</span>
+          <div className="star-rating">
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+          </div>
+        </div>
+        <h3>AGENT X</h3>
+        <p>암흑조직의 음모를 막아라!</p>
+      </div>
+      <div className="item">
+        <img src={gameImage} className="game-image" alt="gameImage" />
+        <div className="detail-content">
+          <span className="location">서울숲 일대</span>
+          <div className="star-rating">
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+            <img src={star} alt="star" />
+          </div>
+        </div>
+        <h3>AGENT X</h3>
+        <p>암흑조직의 음모를 막아라!</p>
+      </div>
     </Styled.Container>
   );
 };
--- src/components/GameItem/index.styles.js
+++ src/components/GameItem/index.styles.js
@@ -1,16 +1,46 @@
 import styled from "styled-components";
 
 const Container = styled.section`
-  width: 420px;
-  background: green;
-  position: relative;
+  display: flex;
+  position: absolute;
+  left: 13%;
 
-  > .game-image {
-    width: 100%;
-    border-radius: 10px;
-    position: absolute;
-    top: 0;
-    left: 0;
+  > .item {
+    width: 420px;
+    position: relative;
+    margin-right: 20px;
+
+    > .game-image {
+      width: 100%;
+      border-radius: 10px;
+      top: 0;
+      left: 0;
+    }
+
+    > .detail-content {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 15px;
+
+      > .location {
+        color: #626262;
+        font-weight: 500;
+        font-size: 16px;
+      }
+    }
+
+    > h3 {
+      font-size: 1.8rem;
+      font-weight: 500;
+      margin-bottom: 12px;
+    }
+
+    > p {
+      color: #626262;
+      font-weight: 500;
+      font-size: 16px;
+    }
   }
 `;
 
