8608e83628fd8841e739e516832e47980be51020
Meagle <mindfulyeon@gmail.com>
2020-08-27 11:07:43 +0900

    [스크롤 이벤트리스너 추가] Home 컴포넌트 > pageYOffset 스테이트 관리

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Introduction from "../components/Introduction";
 import Games from "../components/Games/index";
 
 const Home = () => {
+  const [pageYOffset, setPageYOffset] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setPageYOffset(window.pageYOffset);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    console.log("pageYOffset", pageYOffset);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [pageYOffset]);
+
   return (
     <>
       <Header />
