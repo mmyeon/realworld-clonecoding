1a9103d8e605a6f361009753682de08518712ecf
Meagle <mindfulyeon@gmail.com>
2020-09-04 15:50:30 +0900

    [리팩토링] 불필요한 코드 삭제

--- src/components/GameItem/index.js
+++ src/components/GameItem/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import React from "react";
 import Styled from "./index.styles.js";
-// import gameImage from "../../images/game-collection/agent-x.png";
 import star from "../../images/game-collection/star.svg";
 
 const GameItem = ({ item }) => {
@@ -11,8 +9,7 @@ const GameItem = ({ item }) => {
 
   return (
     // TODO:item.id가 6이면 다시 map돌리기
-    <Styled.Container className="item" key={item.id}>
-      {console.log(item.id)}
+    <Styled.Container className="item">
       <img src={item.image} className="game-image" alt="gameImage" />
       <div className="detail-content">
         {item.location.length > 20 ? (
@@ -30,7 +27,6 @@ const GameItem = ({ item }) => {
       </div>
       <h3>{item.title}</h3>
       <p>{item.titleDetail}</p>
-      {/* // {item.id === 6 ? console.log("okay") : console.log("nope")} */}
     </Styled.Container>
   );
 };
