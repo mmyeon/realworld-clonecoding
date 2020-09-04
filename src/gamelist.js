8a1b24a772afdb47478ad8f3d5e49de4ed228796
Meagle <mindfulyeon@gmail.com>
2020-09-04 15:29:08 +0900

    [리팩토링] 파일이름 카멜케이스로 변경 / id 프로퍼티 삭제

--- src/gamelist.js
+++ src/gamelist.js
@@ -5,16 +5,14 @@ import midnight from "../src/images/game-collection/midnight.jpg";
 import secret from "../src/images/game-collection/secret.jpg";
 import miracle from "../src/images/game-collection/miracle.jpg";
 
-const gamelist = [
+const gameList = [
   {
-    id: 1,
     image: agentX,
     location: "서울숲 일대",
     title: "AGENT X",
     titleDetail: "암흑조직의 음모를 막아라!",
   },
   {
-    id: 2,
     image: lastCollection,
     location: "동대문 일대",
     title: "라스트 콜렉션",
@@ -22,7 +20,6 @@ const gamelist = [
       "패션의 메카 동대문에서 스승님의 메시지를 찾고 컬렉션을 완성하라!",
   },
   {
-    id: 3,
     image: jungdong,
     location: "서울 정동 일대",
     title: "정동 밀서",
@@ -30,21 +27,18 @@ const gamelist = [
       "🎉재오픈🎉 정동에 숨겨진 비밀을 풀고 위기에 처한 독립운동을 지켜라!",
   },
   {
-    id: 4,
     image: midnight,
     location: "온라인",
     title: "Midnight Train",
     titleDetail: "당신은 지금 어디쯤에 있나요?",
   },
   {
-    id: 5,
     image: secret,
     location: "서울 / 전주 / 부산",
     title: "태양단의 비밀",
     titleDetail: "한반도를 지키는 9개의 국보를 찾아라! 서울 / 전주 / 부산",
   },
   {
-    id: 6,
     image: miracle,
     location: "서울극장 (서울특별시 종로구 돈화문로 13) 일대",
     title: "미라클",
@@ -52,4 +46,4 @@ const gamelist = [
   },
 ];
 
-export default gamelist;
+export default gameList;
